b3dc2731d8e16e0e34f5b4c2a0eb4e818c0ce8c7
john <john@email.com>
2025-04-13 22:49:22 -0400

    Trimmed extra modonomicon datagen files
    Fixed build.gradle modonomicon dependency to give access to datagen resources. This is against the recommendation of the modonomicon documentation but datagen wouldn't run unless I did this.

--- src/main/java/com/jc/bestiary/datagen/DataGenerators.java
+++ src/main/java/com/jc/bestiary/datagen/DataGenerators.java
@@ -6,9 +6,7 @@
 
 package com.jc.bestiary.datagen;
 
-import com.jc.bestiary.datagen.book.AddToDemoBook;
 import com.jc.bestiary.datagen.book.DemoBook;
-import com.jc.bestiary.datagen.book.DemoLeaflet;
 import com.klikli_dev.modonomicon.Modonomicon;
 import com.klikli_dev.modonomicon.api.datagen.AddToModonomiconLanguageProvider;
 import com.klikli_dev.modonomicon.api.datagen.LanguageProviderCache;
@@ -29,11 +27,7 @@ public class DataGenerators {
 
         generator.addProvider(event.includeServer(), NeoBookProvider.of(event,
                         //Add our demo book sub provider to the book provider
-                        new DemoBook(Modonomicon.MOD_ID, enUsCache),
-                        //Add our demo leaflet sub provider to the book provider
-                        new DemoLeaflet(Modonomicon.MOD_ID, enUsCache),
-                        //Add our addon book provider which adds to theurgy's book
-                        new AddToDemoBook(addToLangCache)
+                        new DemoBook(Modonomicon.MOD_ID, enUsCache)
                 )
         );
         //Important: lang provider needs to be added after the book provider, so it can read the texts added by the book provider out of the cache
